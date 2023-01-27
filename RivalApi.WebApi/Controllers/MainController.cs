9a73f7cd95c8a4b4f6060e1bda2657feb91d49c9
rival hayrullov <rival.hayrullov@simbirsoft.com>
2023-01-27 14:13:07 +0300

    First commit

--- RivalApi.WebApi/Controllers/MainController.cs
+++ RivalApi.WebApi/Controllers/MainController.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace RivalApi.WebApi.Controllers;
 
-public class MainController
+public class MainController : ControllerBase
 {
-    
+    [HttpGet]
+    [Route("GetString")]
+    public string GetString()
+    {
+        return "method";
+    }
 }
